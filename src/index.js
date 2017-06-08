4cbd540189182e4a20d30f3a119d105bfa754a2d
Tobias Årud <me@tobiasarud.se>
2017-06-08 17:03:40 +0200

    further development, mainly experience section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import styles from '../sass/style.scss';
+import { replaceTextEn } from './lang_en';
---
+++ src/lang_en.js
@@ -0,0 +1,7 @@
+function replaceTextEn(){
+    const h2title = document.getElementsByClassName('title');
+    const newH2title = h2title.innerHTML('Front-end Developer / Wordpress developer');
+    return newH2title;
+}
+
+export { replaceTextEn };
